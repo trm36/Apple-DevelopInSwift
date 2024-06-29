a06a7470b40470671ac74b3b6b44d6e2499137d2
Taylor Mott <trm36@humboldt.edu>
2024-06-29 13:15:04 -0700

    Update Finder Structure

---
+++ Develop in Swift/SeaCreatures/SeaCreatures/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SeaCreatures
+//
+//  Created by Taylor on 6/26/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "SlugScene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ Develop in Swift/SeaCreatures/SeaCreatures/SeaCreaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeaCreaturesApp.swift
+//  SeaCreatures
+//
+//  Created by Taylor on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SeaCreaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

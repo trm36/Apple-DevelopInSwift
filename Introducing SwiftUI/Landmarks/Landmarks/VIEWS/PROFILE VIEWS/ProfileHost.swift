33d396209a98137c3a38510d4c0ef47e432f1d3d
Taylor Mott <trm36@humboldt.edu>
2024-07-08 23:59:10 -0700

    Working with UI Controls

--- Introducing SwiftUI/Landmarks/Landmarks/VIEWS/PROFILE VIEWS/ProfileHost.swift
+++ Introducing SwiftUI/Landmarks/Landmarks/VIEWS/PROFILE VIEWS/ProfileHost.swift
@@ -19,15 +19,27 @@ struct ProfileHost: View {
     var body: some View {
         VStack(alignment: .leading, spacing: 20.0) {
             HStack {
+                if editMode?.wrappedValue == .active {
+                    Button("Cancel", role: .cancel) {
+                        draftProfile = modelData.profile
+                        editMode?.animation().wrappedValue = .inactive
+                    }
+                }
                 Spacer()
                 EditButton()
             }
+            
             if editMode?.wrappedValue == .inactive {
                 ProfileSummary(profile: modelData.profile)
             } else {
-                Text("Profile Editor")
+                ProfileEditor(profile: $draftProfile)
+                    .onAppear {
+                        draftProfile = modelData.profile
+                    }
+                    .onDisappear {
+                        modelData.profile = draftProfile
+                    }
             }
-            
         }
         .padding()
     }
